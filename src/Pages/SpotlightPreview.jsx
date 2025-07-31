fab6890e929ff1a743bf55cd7a595abd2976ca10
Hardeylord <adedipupoadeagbo@gmail.com>
2025-07-31 21:31:27 +0100

    height update

--- src/Pages/SpotlightPreview.jsx
+++ src/Pages/SpotlightPreview.jsx
@@ -6,7 +6,7 @@ import "./avatar.css";
 
 export function SpotlightPreview() {
   return (
-    <div className="relative flex h-fit md:h-screen w-full overflow-hidden bg-[#00020F] antialiased md:items-center md:justify-center">
+    <div className="relative flex h-[100vh] md:h-screen w-full overflow-hidden bg-[#00020F] antialiased md:items-center md:justify-center">
       <div
         className={cn(
           "absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"
